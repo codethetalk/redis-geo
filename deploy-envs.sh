34a9c51169ea9881a2f0371486001fdefa56d877
codethetalk <codethetalk@gmail.com>
2017-04-17 21:02:04 +0530

    creds

--- deploy-envs.sh
+++ deploy-envs.sh
@@ -4,7 +4,7 @@
 export IMAGE_NAME=netcoreapps-redisgeo
 export IMAGE_VERSION=latest
 
-export AWS_DEFAULT_REGION=eu-central-1b
+export AWS_DEFAULT_REGION=eu-central-1
 export AWS_ECS_CLUSTER_NAME=default
 export AWS_VIRTUAL_HOST=52.29.2.206
 
